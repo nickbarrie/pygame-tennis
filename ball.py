8226b9acd7bfd15796b9fe27fea725c3bb6b543c
Nick <nickbarrie12@gmail.com>
2024-10-15 20:25:36 -0400

    optimze ball

--- ball.py
+++ ball.py
@@ -3,6 +3,17 @@ from settings import WINDOW_WIDTH, WINDOW_HEIGHT, WHITE, SCALE_FACTOR, SPRITE_SI
 
 
 class Ball(pygame.sprite.Sprite):
+    drag_coefficient = 0.995
+    cutoff_speed = 0.1 
+    gravity = 0.04
+    bounce_resitution = -0.9
+    bounce_resitution_net = -0.2
+
+                    
+    min_z = 0
+    max_z = 50
+    min_radius = 10
+    max_radius = 20
 
     
 
@@ -11,39 +22,36 @@ class Ball(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.z = z
+
         self.radius = radius
+
         self.speed_x = 0
         self.speed_y = 0
         self.speed_z = 0
+
         self.served = False
         self.last_hit_time = 0 
         self.hit_cooldown = 500  
         self.bounce_count = 0
         self.in_play = False
         self.angle = 0
+
        
         if sprite_sheet is not None:
             self.image = pygame.Surface((SPRITE_SIZE, SPRITE_SIZE), pygame.SRCALPHA)
             self.image.blit(sprite_sheet, (0, 0), (5 * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE))
-
-            self.rect = self.image.get_rect()  # Create a rect from the image size
-            self.rect.center = (x, y)  # Set initial position
-        else:
-            self.rect = pygame.Rect(x - radius, y - radius, radius , radius )
-            self.rect.center = (x, y)
 
     def check_net_collision(self, net):
         if self.z <= net.height:
             ball_rect = pygame.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
             net_rect = pygame.Rect(net.x, net.y, net.width, net.height)
             if ball_rect.colliderect(net_rect):
-                self.speed_y *= -0.2
+                self.speed_y *= self.bounce_resitution_net
                 return True
         return False
 
     def serve(self, speed_x, speed_y, speed_z, player_x, player_y):
         if not self.served:
-            print("Ball served")
             self.x = player_x
             self.y = player_y
             self.z = 1
@@ -60,32 +68,26 @@ class Ball(pygame.sprite.Sprite):
         return self.speed_z == 0 and self.z == 0 and self.speed_x == 0 and self.speed_y == 0
 
     def apply_friction(self):
-        drag_coefficient = 0.997  # Simulate friction or air resistance
-        cutoff_speed = 0.1 
-        
-        self.speed_x *= drag_coefficient
-        self.speed_y *= drag_coefficient
+        self.speed_x *= self.drag_coefficient
+        self.speed_y *= self.drag_coefficient
 
-        self.speed_x *= drag_coefficient
-        self.speed_y *= drag_coefficient
-
-        if abs(self.speed_x) < cutoff_speed:
+        if abs(self.speed_x) < self.cutoff_speed:
             self.speed_x = 0
-        if abs(self.speed_y) < cutoff_speed:
+        if abs(self.speed_y) < self.cutoff_speed:
             self.speed_y = 0
-        if abs(self.speed_z) < cutoff_speed and self.z <= 0.02:
+        if abs(self.speed_z) < self.cutoff_speed and self.z <= 0.02:
             self.speed_z = 0
             self.z = 0
 
     
     def apply_gravity(self):
         if self.z > 0:
-            self.speed_z -= 0.035
+            self.speed_z -= self.gravity
 
 
     def apply_bounce(self):
         if self.z < 0:  
-            self.speed_z *= -0.9
+            self.speed_z *= self.bounce_resitution
             self.bounce_count += 1 
 
     def apply_spin(self):
@@ -102,8 +104,6 @@ class Ball(pygame.sprite.Sprite):
         self.x += self.speed_x
         self.y += self.speed_y
         self.z += self.speed_z
-
-        self.rect.center = (self.x, self.y)
         
         self.apply_bounce()
         self.apply_friction()
@@ -112,42 +112,29 @@ class Ball(pygame.sprite.Sprite):
 
             
 
-    def scale_radius(self, min_z, max_z, min_radius, max_radius):
-        z = max(min_z, min(max_z, self.z))  
-        return min_radius + (max_radius - min_radius) * ((z - min_z) / (max_z - min_z))
+    def scale_radius(self):
+        z = max(self.min_z, min(self.max_z, self.z))  
+        return self.min_radius + (self.max_radius - self.min_radius) * ((z - self.min_z) / (self.max_z - self.min_z))
 
-    def draw(self,screen):
-        
-        min_z = 0
-        max_z = 50
-        min_radius = 10
-        max_radius = 20
-        scaled_radius = self.scale_radius(min_z, max_z, min_radius, max_radius)
 
+    def draw(self, screen):
+        scaled_radius = self.scale_radius()
 
         scaled_image = pygame.transform.scale(self.image, (int(SCALE_FACTOR * scaled_radius), int(SCALE_FACTOR * scaled_radius)))
         scaled_rotated_image = pygame.transform.rotate(scaled_image, self.angle)
         rotated_rect = scaled_rotated_image.get_rect(center=(self.x, self.y))
 
+        shadow_width = max(1, int(scaled_radius * (2 - self.z / self.max_z)))  
+        shadow_height = max(1, int(scaled_radius * .7 * (1.2 - self.z / self.max_z)))
+        shadow_color = (0, 0, 0, max(0, min(255, int(150 * (1 - self.z / self.max_z)))))
 
-            # Draw the shadow
-        shadow_width = max(1, int(scaled_radius * (2 - self.z / max_z)))  
-        shadow_height = max(1, int(scaled_radius * .7 * (1.2 - self.z / max_z)))
-
-        shadow_color = (0, 0, 0, max(0, min(255, int(150 * (1 - self.z / max_z)))))
-
-        
-        # Create a surface for the shadow with alpha transparency
         shadow_surface = pygame.Surface((shadow_width, shadow_height), pygame.SRCALPHA)
         pygame.draw.ellipse(shadow_surface, shadow_color, shadow_surface.get_rect())
-        
-        # Calculate shadow position (directly below the ball)
-        shadow_x = self.x - shadow_width // 2
-        shadow_y = self.y + scaled_radius // 2 + self.z * SCALE_FACTOR  # The shadow is slightly below the ball's y-position
-        
-        # Draw the shadow on the screen
-        screen.blit(shadow_surface, (shadow_x, shadow_y))
 
+        shadow_x = self.x - shadow_width // 2
+        shadow_y = self.y + scaled_radius // 2 + self.z * SCALE_FACTOR
+
+        screen.blit(shadow_surface, (shadow_x, shadow_y))
         screen.blit(scaled_rotated_image, rotated_rect.topleft)
 
     def can_be_hit(self):
